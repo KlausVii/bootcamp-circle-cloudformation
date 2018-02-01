1f1a945f4f758694f3f9a27e8fb47c7d974656e8
KlausVii <klaus.viisainen@ovoenergy.com>
2018-02-01 15:14:47 +0000

    add circle workflow

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,5 +1,10 @@
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.auth.{AWSCredentialsProviderChain, EC2ContainerCredentialsProviderWrapper, EnvironmentVariableCredentialsProvider, SystemPropertiesCredentialsProvider}
+import com.amazonaws.auth.{
+  AWSCredentialsProviderChain,
+  EC2ContainerCredentialsProviderWrapper,
+  EnvironmentVariableCredentialsProvider,
+  SystemPropertiesCredentialsProvider
+}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.gu.scanamo._
 import controllers.TodoController
@@ -10,7 +15,8 @@ import play.api.routing.Router
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
 import router.Routes
 
-class AppComponents(context: ApplicationLoader.Context) extends BuiltInComponentsFromContext(context) {
+class AppComponents(context: ApplicationLoader.Context)
+    extends BuiltInComponentsFromContext(context) {
   // Creds provider chain that looks in both the "training" and default profiles
   val awsCredsProvider = new AWSCredentialsProviderChain(
     new EnvironmentVariableCredentialsProvider(),
@@ -20,16 +26,23 @@ class AppComponents(context: ApplicationLoader.Context) extends BuiltInComponent
     new EC2ContainerCredentialsProviderWrapper()
   )
 
-  val dynamoClient = AmazonDynamoDBClient.builder().withCredentials(awsCredsProvider).build()
+  val dynamoClient = AmazonDynamoDBClient
+    .builder()
+    .withCredentials(awsCredsProvider)
+    .withRegion("eu-west-1")
+    .build()
   val tableName = configuration.get[String]("dynamo.table")
   val dynamoTable: Table[TodoItem] = Table[TodoItem](tableName)
 
-  val todoController = new TodoController(controllerComponents, dynamoClient, new DynamoOps(dynamoTable))
+  val todoController = new TodoController(controllerComponents,
+                                          dynamoClient,
+                                          new DynamoOps(dynamoTable))
 
   override def router: Router = new Routes(httpErrorHandler, todoController)
   override def httpFilters: Seq[EssentialFilter] = Nil
 }
 
 class AppLoader extends ApplicationLoader {
-  override def load(context: ApplicationLoader.Context): Application = new AppComponents(context).application
+  override def load(context: ApplicationLoader.Context): Application =
+    new AppComponents(context).application
 }
